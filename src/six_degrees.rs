97710a718912b0eace770ad0ea92ffc6dbfcd9ca
chisamats <chisamatsumoto1213@gmail.com>
2024-12-10 11:41:02 -0500

    Update six_degrees.rs

--- src/six_degrees.rs
+++ src/six_degrees.rs
@@ -3,28 +3,40 @@ use std::collections::VecDeque;
 
 type Graph = HashMap<usize, usize>;
 
-pub fn out_degree_adjacency_list(graph: &Graph) -> HashMap<usize, Vec<usize>> {
+pub fn out_degree_adj_list(graph: &Vec<(usize, usize)>) -> HashMap<usize, Vec<usize>> {
     let mut adj_list: HashMap<usize, Vec<usize>> = HashMap::new();
 
-    for (&node, &neighbor) in graph {
-        adj_list.entry(neighbor).or_insert_with(Vec::new).push(node);
+    for (node, neighbor) in graph {
+        adj_list.entry(*node).or_insert_with(Vec::new).push(*neighbor);
     }
 
-    adj_list
+    let keys: Vec<usize> = adj_list.keys().cloned().collect();
+    let half_len = keys.len() / 10;
+
+    let mut part_adj_list: HashMap<usize, Vec<usize>> = HashMap::new();
+    for i in 0..half_len {
+        if let Some(key) = keys.get(i) {
+            if let Some(value) = adj_list.get(key) {
+                part_adj_list.insert(*key, value.clone());
+            }
+        }
+    }
+
+    part_adj_list
 }
 
 //bfs six degrees of separation
-fn bfs_degrees_of_separation(graph: &HashMap<usize, Vec<usize>>, start: usize) -> HashMap<usize, usize> {
+fn bfs_degrees_of_separation(graph: &HashMap<usize, Vec<usize>>, start: usize) -> Graph {
     let mut queue = VecDeque::new();
     let mut distances = HashMap::new();
     distances.insert(start, 0);
     queue.push_back(start);
 
-    while let Some(v) = queue.pop_front() { // new unprocessed vertex
+    while let Some(v) = queue.pop_front() { 
         let current_dist = distances[&v];
         if let Some(neighbors) = graph.get(&v) {
             for u in neighbors {
-                if !distances.contains_key(u) { // consider all unprocessed neighbors of v
+                if !distances.contains_key(u) { 
                     let new_dist = current_dist + 1;
                     distances.insert(*u, new_dist);
                     queue.push_back(*u);
@@ -36,24 +48,25 @@ fn bfs_degrees_of_separation(graph: &HashMap<usize, Vec<usize>>, start: usize) -
     distances
 }
 
-pub fn graph_degrees_of_separation(edges: &HashMap<usize, usize>) {
-    let graph = out_degree_adjacency_list(&edges);
-    let nodes = graph.len();
+pub fn graph_degrees_of_separation(edges: &Vec<(usize, usize)>) {
+    //HashMap<node, Vec<neighbors>>
+    let graph = out_degree_adj_list(&edges);
     let mut count = 0;
     let mut all_distances = 0;
-    for n in 0..nodes {
-        let distances = bfs_degrees_of_separation(&graph, n);
-        all_distances += distances.len();
-        for m in 0..nodes {
+    for n in graph.keys() {
+        //HashMap<node, degree of sep from n>
+        let distances = bfs_degrees_of_separation(&graph, *n);
+        for m in graph.keys() {
+            all_distances += 1;
             if let Some(&distance) = distances.get(&m) {
                 if distance > 6 {
-                     count += 1;
+                    count += 1;
                 }
             }
         }
     }
 
-    println!("{}/{} = {} paths between nodes are greater than 6", count, all_distances, count as f64/all_distances as f64);
+    println!("{}/{} = {:.4} paths between nodes are greater than 6", count, all_distances, count as f64/all_distances as f64);
 }
 
 
